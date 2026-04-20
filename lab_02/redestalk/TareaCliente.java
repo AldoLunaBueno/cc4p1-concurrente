14140c07c3d346c515d27517509ad4febad4c45d
AldoLunaBueno <albanovaphi7@gmail.com>
2026-04-19 22:32:28 -0500

    Implementación de dialogo entre servidor y clientes

---
+++ lab_02/redestalk/TareaCliente.java
@@ -0,0 +1,72 @@
+package uni.cc4p1.labo_02.redesOk;
+
+import java.util.Scanner;
+
+class TareaCliente
+{
+    TCPClient50 mTcpClient;
+    Scanner sc;
+    public static void main(String[] args)  {
+        TareaCliente objcli = new TareaCliente();
+        objcli.iniciar();
+    }
+    void iniciar(){
+       new Thread(
+            new Runnable() {
+
+                @Override
+                public void run() {
+                    mTcpClient = new TCPClient50("127.0.0.1",
+                        new TCPClient50.OnMessageReceived(){
+                            @Override
+                            public void messageReceived(String message){
+                                ClienteRecibe(message);
+                                Integer suma = procesarComando(message);
+                                if (suma == null) return;
+                                ClienteEnvia(suma.toString());
+                            }
+                        }
+                    );
+                    mTcpClient.run();                   
+                }
+            }
+        ).start();
+        //---------------------------
+       
+        String salir = "n";
+        sc = new Scanner(System.in);
+        System.out.println("Cliente bandera 01");
+        while( !salir.equals("s")){
+            salir = sc.nextLine();
+            ClienteEnvia(salir);
+        }
+        System.out.println("Cliente bandera 02");
+    
+    }
+    void ClienteRecibe(String llego){
+        System.out.println("CLINTE50 El mensaje::" + llego);
+
+    }
+    void ClienteEnvia(String envia){
+        if (mTcpClient != null) {
+            mTcpClient.sendMessage(envia);
+        }
+    }
+
+    Integer procesarComando(String mensaje) {
+        String[] comandoParseado = mensaje.split(" ");
+        switch (comandoParseado[0]) {
+            case "sumar":
+                System.out.println("Sumando...");
+                if (comandoParseado.length != 3) {
+                    System.out.println("Error: solo se suma dos números, así: sumar 1 2");
+                    return null;
+                }
+                return (int) Integer.valueOf(comandoParseado[1]) + Integer.valueOf(comandoParseado[2]);
+        
+            default:
+                return 0;
+        }
+    }
+
+}
---
+++ lab_02/redestalk/TareaServidor.java
@@ -0,0 +1,49 @@
+package uni.cc4p1.labo_02.redesOk;
+
+import java.util.Scanner;
+
+public class TareaServidor {
+   TCPServer50 mTcpServer;
+   Scanner sc;
+   public static void main(String[] args) {
+       TareaServidor objser = new TareaServidor();
+       objser.iniciar();
+   }
+   void iniciar(){
+       new Thread(
+            new Runnable() {
+
+                @Override
+                public void run() {
+                      mTcpServer = new TCPServer50(
+                        new TCPServer50.OnMessageReceived(){
+                            @Override
+                            public void messageReceived(String message){
+                                ServidorRecibe(message);
+                            }
+                        }
+                    );
+                    mTcpServer.run();                   
+                }
+            }
+        ).start();
+        //-----------------
+        String salir = "n";
+        sc = new Scanner(System.in);
+        System.out.println("Servidor bandera 01");
+        while( !salir.equals("s")){
+            salir = sc.nextLine();
+            ServidorEnvia(salir);
+       }
+       System.out.println("Servidor bandera 02"); 
+   
+   }
+   void ServidorRecibe(String llego){
+       System.out.println("SERVIDOR40 El mensaje:" + llego);
+   }
+   void ServidorEnvia(String envia){
+        if (mTcpServer != null) {
+            mTcpServer.sendMessageTCPServer(envia);
+        }
+   }
+}
