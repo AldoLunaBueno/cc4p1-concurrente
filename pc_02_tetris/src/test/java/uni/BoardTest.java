3b5ce81a47a7319ed0c1e748cdc0c24185983fd9
AldoLunaBueno <albanovaphi7@gmail.com>
2026-04-20 12:39:45 -0500

    Avance inicial del proyecto de tetris de n jugadores (PC2)

---
+++ pc_02_tetris/src/test/java/uni/BoardTest.java
@@ -0,0 +1,39 @@
+package uni;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board(20, 10); // filas: 0-19, columnas: 0-9
+    }
+    @Test
+    void testEmptyBoard() {
+        assertEquals(board.getState(7, 8), 0); // el estado vacío es 0
+    }
+
+    @Test
+    void testBoardBounds() {
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            board.getState(-1, 0);            
+        });
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            board.getState(0, 10);
+        });
+    }
+
+    @Test
+    void testLockBlock() {
+        int idJugador = 1;
+        board.lockBlock(19, 5, idJugador);
+        assertEquals(board.getState(19, 5), idJugador);
+        assertEquals(board.getState(19, 4), 0);
+        assertEquals(board.getState(19, 6), 0);
+    }
+}
