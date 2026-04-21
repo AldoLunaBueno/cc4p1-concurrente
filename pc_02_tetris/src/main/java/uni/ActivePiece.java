16d2bd17d13bfe6ad54d6a0f7cf9dc74c93dbf6d
AldoLunaBueno <albanovaphi7@gmail.com>
2026-04-20 22:56:40 -0500

    Implementar un sistema de piezas inmutable ActivePiece representa la pieza en movimiento, la cual tiene coordenadas xy, un índice de rotación y un prototipo de pieza PieceType que define de forma inmutable todas las rotaciones de una pieza de tetris, lo que evita calcular rotaciones.

---
+++ pc_02_tetris/src/main/java/uni/ActivePiece.java
@@ -0,0 +1,66 @@
+package uni;
+
+public class ActivePiece {
+    private PieceType piece;
+    private int player;
+    private int x;
+    private int y;
+    private int rotationIndex;
+    private int rows;
+    private int columns;
+
+    public ActivePiece(PieceType piece, int player, int x, int y) {
+        this.piece = piece;
+        this.player = player;
+        this.rotationIndex = 0;
+        this.x = x;
+        this.y = y;
+        this.rows = piece.getShape(x).length;
+        this.columns = piece.getShape(x)[0].length;
+        
+    }
+
+    // Cambiar estado
+
+    public void moveDown() {
+        this.y++;
+    }
+
+    public void moveLeft() {
+        this.x--;
+    }
+
+    public void moveRight() {
+        this.x++;
+    }    
+
+    public void rotate() {
+        this.rotationIndex = (this.rotationIndex+1) % 4;
+    }  
+
+    // Obtener estado
+
+    public int getPlayer() {
+        return player;
+    }
+
+    public int[][] getCurrentShape() {
+        return piece.getShape(rotationIndex);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int rows() {
+        return rows;
+    }
+
+    public int columns() {
+        return columns;
+    }  
+}
---
+++ pc_02_tetris/src/main/java/uni/PieceType.java
@@ -0,0 +1,39 @@
+package uni;
+
+public enum PieceType {
+    L_PIECE(new int[][][]{
+        {
+            {0, 1},
+            {1, 1}
+        },
+        {
+            {1, 0},
+            {1, 1}
+        },
+        {
+            {1, 1},
+            {1, 0}
+        },
+        {
+            {1, 1},
+            {0, 1}
+        }
+    }),
+    
+    O_PIECE(new int[][][]{
+        {
+            {1, 1},
+            {1, 1}
+        }
+    });
+
+    private final int[][][] rotations;
+
+    PieceType(int[][][] rotations) {
+        this.rotations = rotations;
+    }
+
+    public int[][] getShape(int rotationIndex) {
+        return rotations[rotationIndex % rotations.length];
+    }
+}
