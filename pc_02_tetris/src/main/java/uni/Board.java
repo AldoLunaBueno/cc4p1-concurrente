5f08c28fcfd678f2a0023c441ea2d982269b3c77
AldoLunaBueno <albanovaphi7@gmail.com>
2026-04-20 22:49:45 -0500

    Implementar el espacio de trabajo (1.1) Esta matriz constituye el tablero vacío más las celdas llenas de las piezas de todos los jugadores que ya cayeron y se anclaron.

--- pc_02_tetris/src/main/java/uni/Board.java
+++ pc_02_tetris/src/main/java/uni/Board.java
@@ -1,5 +1,33 @@
 package uni;
 
 public class Board {
-    
+    private final int[][] matrix; // bloques estáticos (ya cayeron)
+    private int rows;
+    private int columns;
+
+    public Board(int rows, int columns) {
+        matrix = new int[rows][columns];
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    public int getState(int i, int j) {
+        return matrix[i][j];
+    }
+
+    public void lockBlock(int i, int j, int player) {
+        matrix[i][j] = player;
+    }
+
+    public int rows() {
+        return rows;
+    }
+
+    public int columns() {
+        return columns;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
