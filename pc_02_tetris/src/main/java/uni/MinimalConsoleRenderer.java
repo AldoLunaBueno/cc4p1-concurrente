0e5f91a26ee88af1b5a9b255414e30c15e522bd1
AldoLunaBueno <albanovaphi7@gmail.com>
2026-04-29 00:13:01 -0500

    Mantener id jugador como entero, pero presentarlo como un clúster de grafemas

--- pc_02_tetris/src/main/java/uni/MinimalConsoleRenderer.java
+++ pc_02_tetris/src/main/java/uni/MinimalConsoleRenderer.java
@@ -50,9 +50,9 @@ public class MinimalConsoleRenderer {
             frameBuffer.append("|");
             for (int x = 0; x < cols; x++) {
                 if (display[y][x] == 0) {
-                    frameBuffer.append(" . ");
+                    frameBuffer.append(" · ");
                 } else {
-                    frameBuffer.append("[").append((char) display[y][x]).append("]");
+                    frameBuffer.append("[").append(PlayerSymbolMapper.getSymbolForId(display[y][x])).append("]");
                 }
             }
             frameBuffer.append("|\n"); // Salto de línea en el buffer
