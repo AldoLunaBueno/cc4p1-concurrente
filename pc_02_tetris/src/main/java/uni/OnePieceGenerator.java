b4b0f65e595ece595180ad8a19ceedc420d7ced4
AldoLunaBueno <albanovaphi7@gmail.com>
2026-04-29 00:14:21 -0500

    Remover variable innecesaria

--- pc_02_tetris/src/main/java/uni/OnePieceGenerator.java
+++ pc_02_tetris/src/main/java/uni/OnePieceGenerator.java
@@ -8,14 +8,11 @@ public class OnePieceGenerator implements PieceGenerator {
     public OnePieceGenerator(int columns, Shape shape) {
         this.columns = columns;
         this.shape = shape;
-        this.count = 0;
     }
 
     @Override
     public Piece createPiece(int playerId) {
-        Piece piece = new Piece(shape, playerId, columns / 2, 0);
-        this.count++;
-        return piece;
+        return new Piece(shape, playerId, columns / 2, 0);
     }
 
     public int getCount() {
