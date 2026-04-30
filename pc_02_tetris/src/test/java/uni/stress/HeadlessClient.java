027d2f3b9e846201a13977ab6ff343740a15c730
AldoLunaBueno <albanovaphi7@gmail.com>
2026-04-29 21:39:02 -0500

    Diferenciar tests unitarios de tests de estrés computacional

---
+++ pc_02_tetris/src/test/java/uni/stress/HeadlessClient.java
@@ -0,0 +1,5 @@
+package uni.stress;
+
+public class HeadlessClient {
+    
+}
--- pc_02_tetris/src/test/java/uni/unit/CollisionEngineTest.java
+++ pc_02_tetris/src/test/java/uni/unit/CollisionEngineTest.java
@@ -1,4 +1,4 @@
-package uni;
+package uni.unit;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
